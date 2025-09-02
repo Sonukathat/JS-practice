490b4e359cb33e4f0fbab8dca97e4f6aa826182c
sonukathat <skkhankathat93@gmail.com>
2025-09-02 10:35:34 +0530

    prime from input

--- primeNumber.js
+++ primeNumber.js
@@ -1,16 +1,27 @@
-let n = 89;
-let c=0;
+const readline = require("readline");
 
-if(n>1){
-    for (let i = 1; n >= i; i++){
-    if(n%i==0){
-        c++;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter a number: ", (num) => {
+    let n = parseInt(num);
+    let c = 0;
+
+    if (n > 1) {
+        for (let i = 1; i <= n; i++) {
+            if (n % i === 0) {
+                c++;
+            }
+        }
     }
-}
-}
 
-if(c==2){
-    console.log("prime number")
-}else{
-    console.log("not prime")
-}
+    if (c === 2) {
+        console.log("prime number");
+    } else {
+        console.log("not prime");
+    }
+
+    rl.close();
+});
