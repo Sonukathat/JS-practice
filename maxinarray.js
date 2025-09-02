cd9971476a4b62ac3825f817455ba0c135485daf
sonukathat <skkhankathat93@gmail.com>
2025-09-02 15:14:10 +0530

    second max with one loop

--- maxinarray.js
+++ maxinarray.js
@@ -11,20 +11,50 @@
 
 // console.log(max);
 
-let arr = [10, 6, 9, 15, 44, 74]
+
+
+
+
+
+
+
+// let arr = [10, 6, 9, 15, 44, 74]
+
+// let max = -Infinity;
+// let smax = -Infinity
+
+// for (let i = 0; i < arr.length; i++) {
+//    if(arr[i]>max){
+//     max=arr[i];
+//    }
+// }
+
+// for(let j=0;j<arr.length;j++){
+//     if(arr[j]<max && arr[j]>smax){
+//         smax=arr[j];
+//     }
+// }
+
+// console.log(max);
+// console.log(smax)
+
+
+
+
+
+
+let arr = [10, 6, 9, 15, 44, 44, 33, 90, 88]
 
 let max = -Infinity;
 let smax = -Infinity
 
 for (let i = 0; i < arr.length; i++) {
-   if(arr[i]>max){
-    max=arr[i];
-   }
-}
-
-for(let j=0;j<arr.length;j++){
-    if(arr[j]<max && arr[j]>smax){
-        smax=arr[j];
+    if (arr[i] > max) {
+        smax = max;
+        max = arr[i];
+    } else {
+        max = max;
+        smax = arr[i];
     }
 }
 
