bddaa2610066dd714a26c06930d1908eb8d17d66
sonukathat <skkhankathat93@gmail.com>
2025-09-02 14:59:43 +0530

    second max

--- maxinarray.js
+++ maxinarray.js
@@ -1,11 +1,32 @@
-let arr = [3, 200, 300, 34, 345, 385, 345, 454, 543, 6543, 2345, 7654, 234, 76, 4, 234, 54, 1, 4, 7]
+// let arr = [3, 200, 300, 34, 345, 385, 345, 454, 543, 6543, 2345, 7654, 234, 76, 4, 234, 54, 1, 4, 7]
 
-max = -Infinity;
+// let max = -Infinity;
+// let smax = -Infinity
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         max = arr[i];
+//     }
+// }
+
+// console.log(max);
+
+let arr = [10, 6, 9, 15, 44, 74]
+
+let max = -Infinity;
+let smax = -Infinity
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-        max = arr[i];
+   if(arr[i]>max){
+    max=arr[i];
+   }
+}
+
+for(let j=0;j<arr.length;j++){
+    if(arr[j]<max && arr[j]>smax){
+        smax=arr[j];
     }
 }
 
 console.log(max);
+console.log(smax)
