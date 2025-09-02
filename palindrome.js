92a2f3601a9f74dd61443ed232e2b5b83e6dbfe2
sonukathat <skkhankathat93@gmail.com>
2025-09-02 10:55:03 +0530

    palindrome with only condition

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,26 @@
-let n = 24542;
+// let n = 24542;
+// let m=n;
+// let pali=0;
+// let res=0;
+
+// for (let i = 0; n > 0; i++) {
+//     pali  = n%10;
+//     res = res*10+pali;
+//     n=Math.floor(n/10);
+// }
+
+// if(m==res){
+//     console.log("palindrome")
+// }else{
+//     console.log("not palindrome");
+// }
+
+let n = 24545;
 let m=n;
 let pali=0;
 let res=0;
 
-for (let i = 0; n > 0; i++) {
+for (;n > 0;) {
     pali  = n%10;
     res = res*10+pali;
     n=Math.floor(n/10);
