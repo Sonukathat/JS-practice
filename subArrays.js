dfed4d5d97e5b0b45c045ffa99df9e5a04fa04a6
sonukathat <skkhankathat93@gmail.com>
2025-09-02 18:25:06 +0530

    max of subarray

--- subArrays.js
+++ subArrays.js
@@ -1,10 +1,26 @@
-let arr = [1, 2, 3, 4, 8, 5, 6, 3, 2];
+let arr = [1, 2, 3, 4];
 
+let result = 0;
+let subArr;
+
+let start=0,end=0;
+
+let j;
 
 for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-        let subArr = arr.slice(i, j + 1);
-        console.log(subArr);
+    let subArrsum = 0;
+    for (j = i; j < arr.length; j++) {
+        subArr = arr.slice(i, j + 1);
+        subArrsum += arr[j]
+        // console.log(subArr);
+        // console.log(subArrsum);
+    }
+    if (subArrsum > result) {
+        result = subArrsum;
+        start=i;
+        end=j;
     }
 }
 
+console.log(result);
+console.log(arr.slice(start,end));
