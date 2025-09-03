128eaf7c935fc93476feffaf6c46af36b60c2d5a
sonukathat <skkhankathat93@gmail.com>
2025-09-03 17:09:15 +0530

    vowel with includes method

--- vowelsCount.js
+++ vowelsCount.js
@@ -1,13 +1,25 @@
-let str = 'helloworld';
-let vowel = 0;
+// let str = 'helloworld';
+// let vowel = 0;
 
-for (let i = 0; i < str.length; i++) {
-    if (
-        str[i] === 'A' || str[i] === 'E' || str[i] === 'I' || str[i] === 'O' || str[i] === 'U' ||
-        str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'
-    ) {
+// for (let i = 0; i < str.length; i++) {
+//     if (
+//         str[i] === 'A' || str[i] === 'E' || str[i] === 'I' || str[i] === 'O' || str[i] === 'U' ||
+//         str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'
+//     ) {
+//         vowel++;
+//     }
+// }
+
+// console.log(vowel); 
+
+let str = 'helloworldA';
+let vowel = 0;
+let vowels = 'AEIOUaeiou';
+
+for (let i=0;i<str.length;i++){
+    if(vowels.includes(str[i])){
         vowel++;
     }
 }
 
-console.log(vowel); 
+console.log(vowel);
