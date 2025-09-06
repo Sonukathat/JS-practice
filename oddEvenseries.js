0094317dd3236fec77a2768ea3b7f7787f7dafc0
sonukathat <skkhankathat93@gmail.com>
2025-09-06 18:51:14 +0530

    odd even series

---
+++ oddEvenseries.js
@@ -0,0 +1,15 @@
+let n=20;
+
+console.log("Even Series");
+for(let i=1;i<=n;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+console.log("Odd Series");
+for(let i=1;i<=n;i++){
+    if(i%2!==0){
+        console.log(i);
+    }
+}
