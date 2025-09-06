1c075c6fd09e092471a5e527e88dd2814ac46a1a
sonukathat <skkhankathat93@gmail.com>
2025-09-06 17:54:53 +0530

    sum of fibonaci series

--- fibonaci.js
+++ fibonaci.js
@@ -1,7 +1,7 @@
 let a = 0;
 let b = 1;
-let n = 20;
-
+let n =10;
+let sum=0
 
 if (n == 0) {
     console.log("enter valid number");
@@ -10,8 +10,11 @@ if (n == 0) {
 } else {
     for (let i = 0; n > i; i++) {
         console.log(a);
+        sum = sum+a;
         let c = a + b;
         b = a;
         a = c;
     }
 }
+
+console.log("This is the Sum of fibonaci series ",sum);
