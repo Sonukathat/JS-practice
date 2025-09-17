7c113037bc99b65f4bd67d6c5c43b70a44dfc37b
sonukathat <skkhankathat93@gmail.com>
2025-09-17 12:45:22 +0530

    one problem added

---
+++ multiplyof3Value.js
@@ -0,0 +1,14 @@
+let arr = [-10,1,9,2,-5,3];
+
+let newArr = arr.map((item)=>Math.abs(item))
+
+let sorted = newArr.sort((a,b)=>a-b);
+
+let sum = 1;
+
+for(let i=sorted.length-1;i>sorted.length-4;i--){
+    sum=sum*sorted[i]
+
+}
+
+console.log(sum)
