d3aff61b82bc4dab4e8078198d956ec04457f260
sonukathat <skkhankathat93@gmail.com>
2025-09-18 19:12:05 +0530

    merge arr from diff method

--- mergeArr.js
+++ mergeArr.js
@@ -1,7 +1,13 @@
 let a1 = [0,3,5,7,9]
-
 let a2 = [1,2,4,6,8]
+let a3 =[]
 
-let a3 = a1.concat(a2);
+for(let i=0;i<a1.length;i++){
+    a3.push(a1[i])
+}
+
+for(let i=0;i<a2.length;i++){
+    a3.push(a2[i])
+}
 
 console.log(a3)
