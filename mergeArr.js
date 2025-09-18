b2d5a38a455e5c32df13a570072f06d4aef34df8
sonukathat <skkhankathat93@gmail.com>
2025-09-18 19:16:30 +0530

    sort arr

--- mergeArr.js
+++ mergeArr.js
@@ -1,6 +1,6 @@
-let a1 = [0,3,5,7,9]
-let a2 = [1,2,4,6,8]
-let a3 =[...a1,...a2]
+// let a1 = [0,3,5,7,9]
+// let a2 = [1,2,4,6,8]
+// let a3 =[...a1,...a2]
 
 // for(let i=0;i<a1.length;i++){
 //     a3.push(a1[i])
@@ -10,6 +10,10 @@ let a3 =[...a1,...a2]
 //     a3.push(a2[i])
 // }
 
+let a1 = [0,3,5,7,9]
+let a2 = [1,2,4,6,8]
+let a3 =[...a1,...a2]
 
+let a4 = a3.sort((a,b)=>a-b)
 
-console.log(a3)
+console.log(a4)
