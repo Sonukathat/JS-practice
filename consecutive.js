7487887cb26b552d3cb5213374109e9eede4117e
sonukathat <skkhankathat93@gmail.com>
2025-09-18 19:02:05 +0530

    consecutive

--- consecutive.js
+++ consecutive.js
@@ -1,18 +1,19 @@
 let arr = [1,1,0,1,3,1,1,1,1];
 
 let max = -Infinity;
+let count=0;
 
 for(let i=0;i<arr.length;i++){
 
-    let count=0;
-
     if(arr[i]===1){
         count++;
+        if(count>max){
+            max=count;
+        }
+    }else{
+        count=0
     }
 
-    if(count>max){
-        max=count
-    }
 }
 
 console.log(max);
