bef6c8a99587f072ca87fbf442c798ff7dfc2abb
sonukathat <skkhankathat93@gmail.com>
2025-09-19 22:47:20 +0530

    do with other method

--- arrValMul.js
+++ arrValMul.js
@@ -1,18 +1,33 @@
 let arr = [0, 2, 3, 4]
 let arr2 = []
 
+// for (let i = 0; i < arr.length; i++) {
+//     let mul = 1;
+//     for (let j = 0; j < arr.length; j++) {
+//         if(arr[i]===arr[j]){
+//             continue;
+//         }else{
+//             mul *= arr[j]
+//         }
+//     }
+
+//     arr2.push(mul);
+
+// }
+
+
+
 for (let i = 0; i < arr.length; i++) {
-    let mul = 1;
+
+    let mul = 1
+    
     for (let j = 0; j < arr.length; j++) {
-        if(arr[i]===arr[j]){
-            continue;
-        }else{
+        if (arr[i] != arr[j]) {
             mul *= arr[j]
         }
     }
 
-    arr2.push(mul);
-
+    arr2.push(mul)
 }
 
 console.log(arr2)
