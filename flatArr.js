3f4fbf39dd3ea2011921364acf0fa6d886c44163
sonukathat <skkhankathat93@gmail.com>
2025-09-20 08:44:12 +0530

    recursion

--- flatArr.js
+++ flatArr.js
@@ -1,7 +1,22 @@
-let arr=[4,1,9,[10,94],98,[46,23],34]
+// let arr=[4,1,9,[10,94],98,[46,23],34]
 
-let arr2 = arr.flat()
+// let arr2 = arr.flat()
 
-let arr3 = arr2.sort((a,b)=>a-b)
+// let arr3 = arr2.sort((a,b)=>a-b)
 
-console.log(arr3)
+// console.log(arr3)
+
+function flat(arr){
+let res =[];
+for(let i=0; i<arr.length; i++){
+    if(Array.isArray(arr[i])){
+        res.push(...flat(arr[i]))
+    }
+    else{
+        res.push(arr[i])
+    }
+}
+return res
+}
+
+console.log(flat([1,2,[3,4]]))
