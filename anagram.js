0d1b29d4e65b727cdffdab98fd139fffa8b3fb5a
sonukathat <skkhankathat93@gmail.com>
2025-09-24 15:04:28 +0530

    anagram

--- anagram.js
+++ anagram.js
@@ -1,8 +1,22 @@
-let str1=listen;
-let str2=silent;
+let str1 = 'listen';
+let str2 = 'silent';
 
-for(let i=0;i<str1.length;i++){
-    for(let j=0;j<str2.length;j++){
-        
+let count = 0;
+
+if(str1.length!==str2.length){
+    return console.log("not")
+}
+
+for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+        if (str1[i] === str2[j]) {
+            count++
+        }
     }
+}
+
+if (count === str1.length) {
+    console.log("anagram");
+} else {
+    console.log("not anagram");
 }
