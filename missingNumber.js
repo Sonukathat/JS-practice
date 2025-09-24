6e668278e9685d8ddc2aaf5a4b4aa0747bb5d116
sonukathat <skkhankathat93@gmail.com>
2025-09-24 07:49:23 +0530

    added

--- missingNumber.js
+++ missingNumber.js
@@ -2,14 +2,14 @@ let arr = [9,2,5,3,8,4,6,1];
 
 let arr2 = arr.sort((a,b)=>a-b);
 
-// for(let i=1; i <= Math.max(...arr); i++){
-//     if(!arr2.includes(i)){
-//         console.log(i);
-//     }
-// }
-
-for(let i=1;i<arr2.length+1;i++){
+for(let i=1; i <= Math.max(...arr); i++){
     if(!arr2.includes(i)){
-        console.log("Missing Number",i);
+        console.log("missing number",i);
     }
 }
+
+// for(let i=1;i<arr2.length+1;i++){
+//     if(!arr2.includes(i)){
+//         console.log("Missing Number",i);
+//     }
+// }
