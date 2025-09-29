80b1445d29c2a6689331517e3718cd68845860ce
sonukathat <skkhankathat93@gmail.com>
2025-09-29 07:37:31 +0530

    addd

---
+++ romantoNumber.js
@@ -0,0 +1,20 @@
+// I=1
+// V=5;
+// X=10
+// L=50
+// C=100
+// D=500
+// M=1000
+
+let str = "XXI"
+
+switch(str){
+    case "XXI":
+      console.log("fist")
+      break;
+    case "XXX":
+        console.log("second");
+        break;
+    default:
+        console.log("third")  
+}
