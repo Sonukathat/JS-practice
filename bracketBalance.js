d08c67cfbecf91f47e3955bfaf68644ea4f3f04a
sonukathat <skkhankathat93@gmail.com>
2025-09-30 16:33:13 +0530

    balance bracket

--- bracketBalance.js
+++ bracketBalance.js
@@ -1,23 +1,49 @@
-function checkBracket(str) {
-    let bracket = {
-        '(': ')',
-        '{': '}',
-        '[': ']'
-    }
+// function checkBracket(str) {
+//     let bracket = {
+//         '(': ')',
+//         '{': '}',
+//         '[': ']'
+//     }
 
-    let store = [];
+//     let store = [];
+
+//     for (let char of str) {
+//         if (char === '(' || char === '{' || char === '[') {
+//             store.push(char);
+//         } else if (char === ')' || char === '}' || char === ']') {
+//             if (store.pop() === bracket[char]) {
+//                 return false
+//             }
+//         }
+//     }
+
+//     return store.length === 0;
+// }
+
+// console.log(checkBracket("{}"))
+
+function isBalanced(str) {
+    const stack = [];
+    const brackets = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
 
     for (let char of str) {
-        if (char === '(' || char === '{' || char === '[') {
-            store.push(char);
-        } else if (char === ')' || char === '}' || char === ']') {
-            if (store.pop() === bracket[char]) {
-                return false
+        if (brackets[char]) {
+            stack.push(brackets[char]);
+        } else {
+            if (stack.pop() !== char) {
+                return false;
             }
         }
     }
-
-    return store.length === 0;
+    return stack.length === 0;
 }
 
-console.log(checkBracket("{}"))
+
+console.log(isBalanced("()][]]"));
+console.log(isBalanced("({[]})"));
+console.log(isBalanced("([)]"));
+console.log(isBalanced("{[}")); 
