25aa86652c673e3626f7f5ca0b375ae0b708b074
sonukathat <skkhankathat93@gmail.com>
2025-10-01 09:23:29 +0530

    leap year

--- leapYear.js
+++ leapYear.js
@@ -1,15 +1,25 @@
 let year = 1900;
 
-if (year % 400 === 0) {
+// if (year % 400 === 0) {
+//     console.log("leap");
+// } else {
+//     if (year % 100 === 0) {
+//         console.log("not");
+//     } else {
+//         if (year % 4 === 0) {
+//             console.log("leap");
+//         } else {
+//             console.log("not");
+//         }
+//     }
+// }
+
+if(year%400===0){
     console.log("leap");
-} else {
-    if (year % 100 === 0) {
-        console.log("not");
-    } else {
-        if (year % 4 === 0) {
-            console.log("leap");
-        } else {
-            console.log("not");
-        }
-    }
+}else if(year%100===0){
+    console.log("not");
+}else if(year%4===0){
+    console.log("leap");
+}else{
+    console.log("not")
 }
