df2083abb5e139713fecda2b17d639dc7c79ea45
sonukathat <skkhankathat93@gmail.com>
2025-10-01 18:51:34 +0530

    subarry

--- subArrays.js
+++ subArrays.js
@@ -1,26 +1,36 @@
-let arr = [1, 2, 3, 4];
+// let arr = [1, 2, 3, 4];
 
-let result = 0;
-let subArr;
+// let result = 0;
+// let subArr;
 
-let start=0,end=0;
+// let start=0,end=0;
 
-let j;
+// let j;
+
+// for (let i = 0; i < arr.length; i++) {
+//     let subArrsum = 0;
+//     for (j = i; j < arr.length; j++) {
+//         subArr = arr.slice(i, j + 1);
+//         subArrsum += arr[j]
+//         // console.log(subArr);
+//         // console.log(subArrsum);
+//     }
+//     if (subArrsum > result) {
+//         result = subArrsum;
+//         start=i;
+//         end=j;
+//     }
+// }
+
+// console.log(result);
+// console.log(arr.slice(start,end));
+
+let arr = [1, 2, 3, 4, 5];
+
 
 for (let i = 0; i < arr.length; i++) {
-    let subArrsum = 0;
-    for (j = i; j < arr.length; j++) {
-        subArr = arr.slice(i, j + 1);
-        subArrsum += arr[j]
-        // console.log(subArr);
-        // console.log(subArrsum);
-    }
-    if (subArrsum > result) {
-        result = subArrsum;
-        start=i;
-        end=j;
+    for (let j = i; j < arr.length; j++) {
+        let subAr = arr.slice(i, j + 1);
+        console.log(subAr);
     }
 }
-
-console.log(result);
-console.log(arr.slice(start,end));
