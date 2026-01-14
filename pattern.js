86b3d53be3c5e386f5ca5a002764be650fd61a61
sonukathat <skkhankathat93@gmail.com>
2026-01-14 15:15:16 +0530

    add pattern

--- pattern.js
+++ pattern.js
@@ -1,7 +1,15 @@
-let n=7;
 
-for(let i=0;i<=7;i++){
-    for(let j=0;j<i;j++){
+
+// for(let i=0;i<=7;i++){
+//     for(let j=0;j<i;j++){
+//         // console.log("*")
+//         process.stdout.write("*");
+//     }
+//     console.log();
+// }
+
+for(let i=7;i>=1;i--){
+    for(let j=1;j<=i;j++){
         // console.log("*")
         process.stdout.write("*");
     }
